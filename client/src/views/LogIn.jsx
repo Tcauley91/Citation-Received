209ed1c7a6bf9c4a3b910ea1c4ec7b332d526ce6
Tcauley91 <caleytyler@gmail.com>
2020-11-17 20:05:22 -0500

    front end db corrections

--- client/src/views/LogIn.jsx
+++ client/src/views/LogIn.jsx
@@ -36,7 +36,7 @@ class LogIn extends React.Component {
 					<div className='column column-33 column-offset-33'>
 						<h1>Log In</h1>
 						<form onChange={this.onInputChange.bind(this)} onSubmit={this.onFormSubmit.bind(this)}>
-							<input type="text" placeholder="BadgeId" name="badgeID" value={badgeId} />
+							<input type="text" placeholder="Badge Id" name="badgeID" value={badgeId} />
 							<input type="password" placeholder="Password" name="password" value={password} />
 							<button>Log In</button>
 						</form>
